894cd8b2ab417c93f8c5810bdab49670b49915ba
Alexander Selzer <aselzer3@gmail.com>
2014-02-02 00:09:54 +0100

    Added README. Created citationer.js. Created help.txt. Packaged module.

---
+++ README.md
@@ -0,0 +1,36 @@
+Citationer - cite differently
+========
+
+```bash
+$ citationer init
+Created citations.json
+
+$ citationer -s en.wikipedia.org/wiki/pH
+pH - Wikipedia, the free encyclopedia. Wikimedia Foundation. Web. Sat Feb 01 2014. <http://en.wikipedia.org/wiki/pH>.
+
+$ citationer -l
+pH - Wikipedia, the free encyclopedia. Wikimedia Foundation. Web. Sat Feb 01 2014. <http://en.wikipedia.org/wiki/pH>. 
+Hydrogen - Wikipedia, the free encyclopedia. Wikimedia Foundation. Web. Sat Feb 01 2014. <http://en.wikipedia.org/wiki/Hydrogen>. 
+
+$ citationer -e citations.txt
+Exported as citations.txt
+
+$ citationer -s en.wikipedia.org/wiki/Node.js --json
+{
+  "title": "Node.js - Wikipedia, the free encyclopedia",
+  "organization": "Wikimedia Foundation",
+  "lastModDate": null,
+  "type": "Web",
+  "accessDate": "Sun Feb 02 2014",
+  "url": "<http://en.wikipedia.org/wiki/Node.js>"
+}
+```
+
+Are you tired of using clunky web sites or word processors
+to manage your references?
+
+Keep them simple, and local.
+
+If a citations.json is found in the current directory, Citationer
+will automatically append all citations to it before printing.
+Otherwise, everything will go to stdout.
---
+++ citationer.js
@@ -0,0 +1,99 @@
+var argv = require("optimist").argv,
+    fs = require("fs"),
+    Citation = require("citation.js");
+
+var style = "MLA",
+    referenceFile;
+
+if (argv.j || argv.json)
+  style = "JSON";
+
+if (fs.existsSync("citations.json"))
+  referenceFile = "citations.json";
+
+if (argv.f || argv.file)
+  referenceFile = argv.f || argv.file;
+
+
+if (process.argv[2] === "init") {
+  if (!fs.existsSync("citations.json")) {
+    try {
+      fs.writeFileSync("citations.json", "[]");
+    }
+    catch(err) {
+      throw err;
+    }
+    console.log("Created citations.json");
+  }
+  else {
+    console.error("citations.json already exists. Refusing to overwrite.");
+  }
+}
+else if (argv.s || argv.site) {
+  var site = argv.s || argv.site;
+  var URI;
+
+  if (!(/^http\:\/\//.test(site)))
+    URI = "http://" + site;
+  else
+    URI = site;
+  
+  var citation = new Citation(URI);
+  citation.getReference(function(err, reference) {
+    if (err) throw err;
+    if (style === "MLA")
+      console.log(citation.convertToMla(reference));
+    else if (style === "JSON")
+      console.log(JSON.stringify(reference, null, 2));
+    if (referenceFile) {
+      try {
+        var references = JSON.parse(fs.readFileSync(referenceFile));
+        references.push(reference)
+        try {
+          fs.writeFileSync(referenceFile, JSON.stringify(references, null, 2));
+        }
+        catch (err) {
+          throw err;
+        }
+      } 
+      catch (err) {
+        throw err;
+      }
+    }
+  });
+}
+else if (argv.l || argv.list) {
+  var citations = JSON.parse(fs.readFileSync(referenceFile));
+  var citer = new Citation();
+  citations.forEach(function(citation) {
+    console.log(citer.convertToMla(citation));
+  });
+}
+else if (argv.e || argv.export) {
+  var exportfile = (argv.e || argv.exportfile);
+  console.log("export:", exportfile);
+  var citations = JSON.parse(fs.readFileSync(referenceFile));
+  var citer = new Citation();
+  var output = "";
+  citations.forEach(function(citation) {
+    output += citer.convertToMla(citation) + "\n";
+  });
+  try {
+    if (typeof(exportfile) === "string")
+      fs.writeFileSync(exportfile, output);
+    else
+      fs.writeFileSync(__dirname + "/citations.txt", output);
+  }
+  catch (err) {
+    throw err;
+  }
+  if (typeof(exportfile) === "string")
+    console.log("Exported as", exportfile);
+  else
+    console.log("Exported as citations.txt");
+}
+else if (process.argv[2] !== "init") {
+  fs.createReadStream(__dirname + "/help.txt")
+  .pipe(process.stdout);
+}
+
